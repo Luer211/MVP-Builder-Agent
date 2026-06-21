549b11b07d2092513aaf2f15f7902e991095b0db
Luer211 <2048478566@qq.com>
2026-06-21 16:21:27 +0800

    support OpenAI API use

--- app/core/llm.py
+++ app/core/llm.py
@@ -1,11 +1,35 @@
 from __future__ import annotations
 
-from typing import Protocol
+from openai import OpenAI
+
+from app.core.config import Settings
 
 
-class LLMClient(Protocol):
+# Todo: 目前只实现了OpenAI的接入，后续应该支持更多模型公司的API
+class LLMClient:
+    def __init__(self, api_key: str, base_url: str, model_name: str, timeout_seconds: int):
+        self.client = OpenAI(
+            api_key=api_key,
+            base_url=base_url,
+            timeout=timeout_seconds,
+        )
+        self.model_name = model_name
+
     def generate(self, prompt: str) -> str:
-        """Generate text for a prompt."""
+        response = self.client.chat.completions.create(
+            model=self.model_name,
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            response_format={"type": "json_object"},
+        )
+
+        content = response.choices[0].message.content
+
+        return content
 
 
 class OpenAILLM:
@@ -21,11 +45,14 @@ class DeepSeekLLM:
         pass
 
     def generate(self, prompt: str) -> str:
-        pass    
+        pass
 
 
-# Todo：app\services\generate_docs.py 调用的时候是 llm = get_llm_client(settings)
-def get_llm_client() -> LLMClient:
-    # return OpenAILLM(...)
-    # return DeepSeekLLM(...)
-    pass
+def get_llm_client(settings: Settings) -> LLMClient:
+    """创建 LLMClient 实例"""
+    return LLMClient(
+        api_key=settings.api_key,
+        base_url=settings.base_url,
+        model_name=settings.model_name,
+        timeout_seconds=settings.request_timeout_seconds,
+    )
